c69522b24dd3b49f6d91651240abac48b1d39404
Nikita Hasan Kafes <kafes-nik@mail.ru>
2022-09-17 15:33:46 +0300

    First week hw

---
+++ src/conditionals.java
@@ -0,0 +1,12 @@
+public class conditionals {
+    public static void main(String[] args){
+        int sayi = 14;
+        if (sayi < 20){
+            System.out.println("Sayı 20'den küçüktür");
+        } else if (sayi == 20){
+            System.out.println("Sayı 20'ye esittir");
+        } else {
+            System.out.println("Sayı 20den buyuktur");
+        }
+    }
+}
