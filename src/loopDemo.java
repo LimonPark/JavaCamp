c467fc4694868c268c4598306fabf9b2d126ffb3
Nikita Hasan Kafes <kafes-nik@mail.ru>
2022-09-17 16:17:59 +0300

    First week hw

---
+++ src/loopDemo.java
@@ -0,0 +1,25 @@
+public class loopDemo {
+    public static void main(String[] args){
+        //For
+        for(int i = 2; i < 10 ; i += 2){
+            System.out.println(i);
+        }
+        System.out.println("Dongu Bitti");
+
+        int i = 2;
+        //While
+        while(i < 10){
+            System.out.println(i);
+            i += 2;
+        }
+        System.out.println("While dongusu bitti");
+
+        //Do-While
+        int j = 100;
+        do{
+            System.out.println(j);
+            j += 2;
+        }while(j < 10);
+        System.out.println("Do-While dongusu bitti");
+    }
+}
