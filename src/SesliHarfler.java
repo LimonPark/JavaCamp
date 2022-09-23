1f2e77f1696d5af9194187bcf936823daf4b1f3f
Nikita Hasan Kafes <kafes-nik@mail.ru>
2022-09-23 13:23:33 +0300

    First week hw

--- src/SesliHarfler.java
+++ src/SesliHarfler.java
@@ -1,2 +1,16 @@
 public class SesliHarfler {
+    public static void main(String[] args){
+        char harf = 'A';
+
+        switch (harf){
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println("Kalin sesli harf");
+                break;
+            default:
+                System.out.println("Ince sesli harf");
+        }
+    }
 }
